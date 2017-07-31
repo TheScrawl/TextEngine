fa434dc1d3155c1c1b6c032c6e8e7695eb94d00b
TheScrawl <TheScrawl@users.noreply.github.com>
2017-08-01 11:21:45 +1200

    Rewrote the npc function in a seperate file, cutting down on bloat and improving readability. Also put npc data into its own file

---
+++ npc.py
@@ -0,0 +1,24 @@
+import npcData
+
+def talk(npc):
+    print(npc.get('name'))
+    print(npc.get('startText'))
+
+    try:
+        if 'exit' in npc.get('functions'): #catch exit function and quit talk
+            print('exiting dialogue')
+            return 0
+        else:
+            pass
+    except TypeError:
+        pass    
+    for option in sorted(npc.keys()):
+        if option != 'name' and option != 'startText' and option != 'functions' and option != 'optionText':
+            print(option + ': ' + npc.get(option).get('optionText'))
+        
+    choice = str(input('> ')).lower()
+    
+    npc = npc.get(choice)
+    talk(npc)
+
+talk(npcData.testNPC)
---
+++ npcData.py
@@ -0,0 +1,48 @@
+testNPC = {
+        'name': 'dickfuck',
+        'startText': 'this is start text',
+
+        'a': 
+            {
+            'optionText': 'this is option a',
+            'startText': 'you chose option a',
+
+            '1': 
+                {
+                'optionText': 'this is option a 1',
+                'startText': 'you chose option a 1',
+
+                '1':
+                    {
+                        'optionText': 'this is option a 1 1',
+                        'startText': 'you chose option a 1 1',
+
+                        'functions': 'exit',
+                    },
+                
+                '2':
+                    {
+                        'optionText': 'this is option a 1 2',
+                        'startText': 'you chose option a 1 2',
+                        
+                        'functions': 'exit',
+                    },
+                },
+
+            '2':
+                {
+                'optionText': 'this is option a 2',
+                'startText': 'you chose option a 2',
+
+                'functions': 'exit',
+                },
+            },
+         
+        'b':
+            {
+            'optionText': 'this is option b',
+            'startText': 'you chose option b',
+            
+            'functions': 'exit',
+            },
+        }
