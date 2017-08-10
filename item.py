c02563d16fb5d6affd97f4c00a65c6a1ea2bdcac
TheScrawl <TheScrawl@users.noreply.github.com>
2017-08-11 09:36:20 +1200

    removed code that had been moved to itemData.py

--- item.py
+++ item.py
@@ -22,16 +22,3 @@ class armour(object):
         self.defence = defence
         self.count = count
 
-#ITEMLISTS (these should be moved to a new file in future, itemData.py or something)
-        
-#Currencies
-gold = item('Gold', 'A small gold coin', 1, 0)
-
-#Quest Items
-shibbledibble = item('Shibbledibble', 'Its shibbledibble, used for a quest', 900000, 0)
-
-#Weapons
-rustyDagger = weapon('Rusty Dagger', 'its a small rusty dagger', 1, 5, 0)
-
-#Armour
-rustyHelmet = armour('Rusty Helmet', 'its a rusty helmet', 1, 5, 0)
