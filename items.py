9630a975175e1b78beda0ae8ccf9878cd3335240
TheScrawl <thescrawlgaming@gmail.com>
2017-08-10 15:07:52 +1200

    fixed all tlygt's shitty code

--- items.py
+++ items.py
@@ -1,45 +1,37 @@
-class Item():
-    def __init__(self, name, description, value):
+class item(object):
+    def __init__(self, name, description, value, count):
         self.name = name
         self.description = description
         self.value = value
+        self.count = count
 
-    def __str__(self):
-        return "{}\n=====\n{}\nValue: {}".format(self.name, self.description, self.value)
-
-class Weapon(Item):
-    def __init__(self, name, description, value, damage):
+class weapon(object):
+    def __init__(self, name, description, value, damage, count):
+        self.name = name
+        self.description = description
+        self.value = value
         self.damage = damage
-        super().__init__(name, description, value)
+        self.count = 0
 
-    def __str__(self):
-        return "{}=====\n{}\nValue: {}\nDamage: {}".format(self.name, self.description, self.value, self.damage)
-
-class Armour(Item):
-    def __init__(self, name, description, value, defence):
+        
+class armour(object):
+    def __init__(self, name, description, value, defence, count):
+        self.name = name
+        self.description = description
+        self.value = value
         self.defence = defence
-        super().__init__(name, description, value)
+        self.count = 0
 
-    def __str__(self):
-        return "{}=====\n{}\nValue: {}\nDefence: {}".format(self.name, self.description, self.value, self.defence)
+#ITEMLISTS (these should be moved to a new file in future, itemData.py or something)
+        
+#Currencies
+gold = item('Gold', 'A small gold coin', 1, 0)
 
-#########################################################################################################################
+#Quest Items
+shibbledibble = item('Shibbledibble', 'Its shibbledibble, used for a quest', 900000, 0)
 
-class Goldcoin(Item):
-    def __init__(self, amt):
-        self.amt = amt
-        super().__init__(
-            name = "Gold coin",
-            description = "A small round coin with {} engraved on the front.".format(str(self.amt)),
-            value = self.amt
-        )
+#Weapons
+rustyDagger = weapon('Rusty Dagger', 'its a small rusty dagger', 1, 5, 0)
 
-class RustyDagger(Weapon):
-    def __init__(self):
-        super().__init__(
-            name = "Rusty Dagger",
-            description = "A once well kept dagger, left to rust.",
-            value = 2
-            damage = 5
-        )
-            
+#Armour
+rustyHelmet = armour('Rusty Helmet', 'its a rusty helmet', 1, 5, 0)
